65074ae9420509f791283cd0cd6be3dc4f0a38f6
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-08-14 19:55:14 +0200

    Restuction
    Added xml for definition for side menu

--- Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlControllers/SideMenuController.java
+++ Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlControllers/SideMenuController.java
@@ -1,7 +1,18 @@
 package de.szut.dqi12.cheftrainer.client.view.fxmlControllers;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,6 +25,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
 import de.szut.dqi12.cheftrainer.client.MainApp;
 import de.szut.dqi12.cheftrainer.client.guiControlling.GUIController;
 import de.szut.dqi12.cheftrainer.client.guiControlling.GUIInitialator;
@@ -36,25 +48,82 @@ public class SideMenuController {
 	public void setMainApp(GUIInitialator mainApp) {
 		this.mainApp = mainApp;
 		GridPane rLayout = mainApp.getRootlayout();
-		Image imageDecline = new Image(
-				MainApp.class
-						.getResourceAsStream("../../../../../images/dummyMenuIcon.png"));
+		generateButtons(((VBox) rLayout.lookup("#sideMenu")));
+	}
 
-		for (Node n : ((VBox) rLayout.lookup("#sideMenu")).getChildren()) {
-			((Button) n).setGraphic(new ImageView(imageDecline));
-			((Button) n).setAlignment(Pos.BASELINE_LEFT);
-			((Button) n).setPrefWidth(expandedWidth);
+	@SuppressWarnings("static-access")
+	private void generateButtons(VBox box) {
+		try {
+			Path buttonDefinitionFile = Paths.get(MainApp.class.getResource(
+					"/definitions/sideMenuButtons.xml").toURI());
+			
+			List<String> xmlLines = Files.readAllLines(buttonDefinitionFile);
+			List<Element> buttonList = parseXMLButtons(xmlLines);
+			List<Button> buttons = new ArrayList<Button>();
+			
+			for (Element e : buttonList) {
+				Button tempButton = generateButtonFromXML(e);
+				buttons.add(tempButton);
+			}
+			
+			box.getChildren().addAll(buttons);
+
+			for (Node n : box.getChildren()) {
+				box.setVgrow(((Button) n), Priority.ALWAYS);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	private Button generateButtonFromXML(Element e){
+		Button tempButton = new Button(e.getChildText("text"));
+		Image image = new Image(
+				MainApp.class.getResourceAsStream("/images/"
+						+ e.getChildText("imageName")));
+		tempButton.setGraphic(new ImageView(image));
+		tempButton.setPrefHeight(250.0);
+		tempButton.setMnemonicParsing(false);
+		tempButton.setAlignment(Pos.BASELINE_LEFT);
+		tempButton.setPrefWidth(expandedWidth);
+		tempButton.setStyle("button");
+
+		if (e.getChildText("triggerButton").equals("true")) {
+			tempButton.setOnAction(this::triggerSideMenu);
+		}
+		else{
+			tempButton.setOnAction(this::buttonPressed);
+		}
+		return tempButton;
+	}
+
+	private List<Element> parseXMLButtons(List<String> xmlStringList) {
+		String xmlString = "";
+		for (String s : xmlStringList) {
+			xmlString += s + "\t";
+		}
+
+		SAXBuilder saxBuilder = new SAXBuilder();
+		List<Element> nodeList = new ArrayList();
+		try {
+			Document doc = saxBuilder.build(new StringReader(xmlString));
+			nodeList = doc.getRootElement().getChildren();
+		} catch (JDOMException e) {
+		} catch (IOException e) {
+		}
+		return nodeList;
 	}
 
 	@FXML
-	public void buttonPressed() {
+	public void buttonPressed(ActionEvent evt) {
 		String fileName = "CommunitiesFrame.fxml";
 		GUIController.getInstance().setContentFrameByName(fileName);
 	}
 
 	@FXML
-	public void triggerSideMenu() {
+	public void triggerSideMenu(ActionEvent evt) {
 		rLayout = mainApp.getRootlayout();
 		ObservableList<Node> buttonList = ((VBox) rLayout.lookup("#sideMenu"))
 				.getChildren();
@@ -76,13 +145,15 @@ public class SideMenuController {
 			((Button) n).setPrefWidth(collapsedWidth);
 		}
 		sideMenuFlag = false;
-		
+
 		ColumnConstraints menuColoum = rLayout.getColumnConstraints().get(0);
 		menuColoum.setMaxWidth(collapsedWidth);
-		
+
 		ColumnConstraints contentColoum = rLayout.getColumnConstraints().get(1);
-		contentColoum.setPrefWidth(rLayout.getWidth()-((VBox)rLayout.lookup("#sideMenu")).getWidth());
-		contentColoum.setMinWidth(rLayout.getWidth()-((VBox)rLayout.lookup("#sideMenu")).getWidth());
+		contentColoum.setPrefWidth(rLayout.getWidth()
+				- ((VBox) rLayout.lookup("#sideMenu")).getWidth());
+		contentColoum.setMinWidth(rLayout.getWidth()
+				- ((VBox) rLayout.lookup("#sideMenu")).getWidth());
 	}
 
 	private void expands(ObservableList<Node> buttonList) {
@@ -93,18 +164,18 @@ public class SideMenuController {
 			((Button) buttonList.get(i)).setText(sideMenuButtonTitles.get(i));
 			((Button) buttonList.get(i)).setPrefWidth(expandedWidth);
 		}
-		sideMenuFlag = true;	
-		
+		sideMenuFlag = true;
 
 		ColumnConstraints menuColoum = rLayout.getColumnConstraints().get(0);
 		menuColoum.setMaxWidth(expandedWidth);
-		
+
 		ColumnConstraints contentColoum = rLayout.getColumnConstraints().get(1);
-		contentColoum.setPrefWidth(rLayout.getWidth()-expandedWidth-100);
-		contentColoum.setMinWidth(rLayout.getWidth()-expandedWidth-100);
+		contentColoum.setPrefWidth(rLayout.getWidth() - expandedWidth - 100);
+		contentColoum.setMinWidth(rLayout.getWidth() - expandedWidth - 100);
 	}
 
 	public List<String> getSideMenuButtonTitles() {
 		return sideMenuButtonTitles;
 	}
+
 }
