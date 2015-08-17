67ac0be395a31e7be08ea7e602b379bb272b427b
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-08-17 21:27:07 +0200

    updated key exchange

--- Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/UserCommunication/ClientHandler.java
+++ Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/UserCommunication/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
@@ -13,18 +14,14 @@ public class ClientHandler implements Runnable {
 	PrintWriter writer;
 	Socket socket;
 	
-	public ClientHandler(Socket clientSocket) {
+	public ClientHandler(Socket clientSocket, Key publicKey) {
 		try {
 			socket = clientSocket;
 			InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
 			writer = new PrintWriter(socket.getOutputStream());
 			reader = new BufferedReader(isReader);
 			
-			KeyPairGenerator kpg = KeyPairGenerator.getInstance( "RSA" );
-			kpg.initialize(1024);
-			KeyPair keyPair = kpg.genKeyPair();
-			
-			writer.println(keyPair.getPublic());
+			writer.println(publicKey);
 			writer.flush();
 		} catch(Exception ex) {ex.printStackTrace();}
 	} 
--- Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/UserCommunication/Server.java
+++ Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/UserCommunication/Server.java
@@ -5,17 +5,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
 public class Server {
 
 	private final int PORT = 5000;
+	private KeyPair keyPair;
+	
+	public Server(){
+		KeyPairGenerator kpg;
+		try {
+			kpg = KeyPairGenerator.getInstance("RSA");
+			kpg.initialize(1024);
+			keyPair = kpg.genKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 	public void run() {
 		try {
 			ServerSocket serverSock = new ServerSocket(PORT);
 			while(true) {
 				Socket clientSocket = serverSock.accept();
-				Thread t = new Thread(new ClientHandler(clientSocket));
+				Thread t = new Thread(new ClientHandler(clientSocket, keyPair.getPublic()));
 				t.start();
 			}
 			
