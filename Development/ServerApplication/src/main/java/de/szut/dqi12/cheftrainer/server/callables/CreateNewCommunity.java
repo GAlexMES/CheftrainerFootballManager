06e568f7aa08f38f5cb8b3fb4f0b17a324653f01
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-09-11 23:19:02 +0200

    integrated Robins code into this branch
    Problems with the width of the table, when framesize is small. #240

--- Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/callables/CreateNewCommunity.java
+++ Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/callables/CreateNewCommunity.java
@@ -1,5 +1,7 @@
 package de.szut.dqi12.cheftrainer.server.callables;
 
-public class CreateNewCommunity {
+import de.szut.dqi12.cheftrainer.connectorlib.callables.CallableAbstract;
+
+public class CreateNewCommunity extends CallableAbstract{
 
 }
