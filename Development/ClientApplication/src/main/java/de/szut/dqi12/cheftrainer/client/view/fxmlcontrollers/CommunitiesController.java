3717b75b5d53e6added5edc1f7116f6f5ee918d8
Robin Bley <robin-bley@hotmail.de>
2015-09-11 20:06:13 +0200

    CommunitiesFrame und Controller angepasst

--- Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlcontrollers/CommunitiesController.java
+++ Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlcontrollers/CommunitiesController.java
@@ -2,12 +2,13 @@ package de.szut.dqi12.cheftrainer.client.view.fxmlcontrollers;
 
 import java.util.ArrayList;
 
-import de.szut.dqi12.cheftrainer.client.gamemanagement.Team;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import de.szut.dqi12.cheftrainer.client.gamemanagement.Team;
 
 public class CommunitiesController {
 
@@ -59,5 +60,30 @@ public class CommunitiesController {
 
 		communitiesFrame.setItems(data);
 
+		this.blabla();
+	}
+
+	public void blabla() {
+		communitiesFrame.setRowFactory( tv -> {
+		    TableRow<Team> row = new TableRow<>();
+		    row.setOnMouseClicked(event -> {
+		        if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+		            Team rowData = row.getItem();
+		            System.out.println(rowData.getCommunityName().get());
+		            //DO SOMETHING
+		            
+		            
+//		            Stage dialogStage = new Stage();
+//		            dialogStage.initModality(Modality.WINDOW_MODAL);
+//		            dialogStage.setScene(new Scene(VBoxBuilder.create().
+//		                children(new Text(rowData.getCommunityName().get()), new Text(rowData.getPlazierung().get()), new Text(rowData.getWertDesTeams().get())).
+//		                alignment(Pos.CENTER).padding(new Insets(5)).build()));
+//		            dialogStage.show();
+		            
+		            
+		        }
+		    });
+		    return row ;
+		});
 	}
 }
