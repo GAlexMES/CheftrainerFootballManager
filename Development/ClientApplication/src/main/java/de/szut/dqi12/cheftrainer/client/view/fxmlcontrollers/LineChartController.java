4344b4173a5a9390613a820aecc0c385cff5da14
Robin Bley <robin-bley@hotmail.de>
2015-10-01 17:51:17 +0200

    Bug fixed in LineChart

--- Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlcontrollers/LineChartController.java
+++ Development/ClientApplication/src/main/java/de/szut/dqi12/cheftrainer/client/view/fxmlcontrollers/LineChartController.java
@@ -33,9 +33,15 @@ public class LineChartController<Y, X> {
 	public void setData(HashMap<String, Integer> data) {
 		Series<String, Integer> series = new Series<String, Integer>();
 		for (String key : data.keySet()) {
-			series.getData().add(new XYChart.Data(key, data.get(key)));
+				
+				series.getData().add(new XYChart.Data<String, Integer>(key, data.get(key)));
 		}
-		this.data.clear();
+		
+		try{			
+			this.data.clear();
+		}catch(Exception e){
+//			e.printStackTrace();
+		}
 		this.data.add(series);
 		this.chart.getData().clear();
 		this.chart.setData(this.data);
@@ -52,7 +58,12 @@ public class LineChartController<Y, X> {
 		this.chart.setData(this.data);
 
 	}
-
+/**
+ * 
+ * @param xValue Wert fuer X-Achse
+ * @param yValue Wert fuer Y-Achse
+ * @param series Die Position des Graphes (0 fuer ersten Graph)
+ */
 	public void addValue(String xValue, int yValue, int series) {
 
 		try {
