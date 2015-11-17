9e492979fdce80778c6c9f9a81a38f3709e86472
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-11-17 16:02:15 +0100

    refactoring and bug fixing
    added callable for the new ID
    refactored poms

---
+++ Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/callables/NewFormationUpdate.java
@@ -0,0 +1,7 @@
+package de.szut.dqi12.cheftrainer.server.callables;
+
+import de.szut.dqi12.cheftrainer.connectorlib.callables.CallableAbstract;
+
+public class NewFormationUpdate extends CallableAbstract {
+
+}
