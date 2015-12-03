fac9cfebbe6e4391aa28f068c8a689ef08c9309b
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-12-03 23:54:55 +0100

    fixed maven, to use the javadoc tag "@custom.position" to link a position in the "Pflichtenheft" to javadoc

--- Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/databasecommunication/PlayerManagement.java
+++ Development/ServerApplication/src/main/java/de/szut/dqi12/cheftrainer/server/databasecommunication/PlayerManagement.java
@@ -21,7 +21,6 @@ import de.szut.dqi12.cheftrainer.server.parsing.TeamParser;
  * first time.
  * 
  * @author Alexander Brennecke
- *
  */
 public class PlayerManagement {
 
@@ -78,7 +77,7 @@ public class PlayerManagement {
 		String sqlQuery = "INSERT INTO Spieler (Name,Verein_ID, Position, Punkte, Marktwert, Nummer, SportalID, Birthday, PicturePath) "
 						+ "VALUES ('"+p.getName()+ "','"
 						+ teamID +"','"
-						+ p.getPositionString() +"','"
+						+ p.getPosition() +"','"
 						+ "0','"
 						+ worth + "','"
 						+ p.getNumber() + "','"
